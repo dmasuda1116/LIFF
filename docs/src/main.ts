e5989dd5528d614dbbafea64e9d93ec1c7e06f53
dmasuda1116 <dmasuda1116@gmail.com>
2023-02-21 14:11:22 +0900

    no message

--- docs/src/main.ts
+++ docs/src/main.ts
@@ -7,11 +7,6 @@ import Button from '@mui/material/Button';
 
 ReactDOM.render(
   <React.StrictMode>
-      <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </Stack>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
